7e07430955cf505de8d5b1ac8d5f9c919e17ecca
Ginger Heef <gingerheef003@gmail.com>
2021-05-09 09:17:31 +0530

    made some changes

--- a.java
+++ a.java
@@ -1,9 +1,5 @@
 public class a{
-    public void fun(int x){
-        if(x>0){
-            fun(--x);
-            System.out.print(x+" ");
-            fun(--x);
-        }
+    public static void main(String args[]){
+        System.out.println("Hello from the Other side...");
     }
 }
